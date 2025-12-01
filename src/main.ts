7c74f47e854f65c2ccb35dced2059d37379ef989
SimeYankulov <jankulov.sime@yahoo.com>
2025-12-01 17:00:40 +0100

    feat(main/create-quiz): implement quiz creation and update test case

--- src/main.ts
+++ src/main.ts
@@ -4,65 +4,37 @@ type PossibleAnswer = {
   correctAnswer: boolean;
 };
 
-type Question = {
+export type Question = {
   id: number;
   label: string;
   possibleAnswers: PossibleAnswer[];
   typeOfQuestion: "SINGLE_QUESTION" | "MULTIPLE_QUESTIONS";
 };
 
-type Quizz = {
+export type Quizz = {
   id: number;
   questions: Question[];
 };
-
-export const createQuestion = (
-  label: string,
-  typeOfQuestion: "SINGLE_QUESTION" | "MULTIPLE_QUESTIONS",
-  possibleAnswers: PossibleAnswer[],
-): Question => ({
-  id,
-  label,
-  typeOfQuestion,
-  possibleAnswers,
-});
-
-export const getQuestion = (id: number): Question => ({
-  id: 1,
-  label: "What is the capital of France?",
-  possibleAnswers: [
-    { id: 1, label: "Paris", correctAnswer: true },
-    { id: 2, label: "Skope", correctAnswer: false },
-    { id: 3, label: "La Tour Eiffel", correctAnswer: false },
-    { id: 4, label: "Lyon", correctAnswer: false },
-  ],
-  typeOfQuestion: "SINGLE_QUESTION",
-});
 
 export type IdGenerator = () => number;
 
 export const idGenerator: IdGenerator = () =>
   Math.floor(Math.random() * 1000000);
 
-// [1;4;6;9]
-export type PickWithoutReplacement = () => number[];
+export type PickWithoutReplacementFunction = () => number[];
 
-export const createQuizz = (
-  idGenerator: IdGenerator,
-  pickWithoutReplacement: PickWithoutReplacement,
-): Quizz => {};
+type GetQuestions = (ids: number[]) => Question[];
+type Dependencies = {
+  idGenerator: IdGenerator;
+  getQuestions: GetQuestions;
+  pickWithoutReplacementFunction: PickWithoutReplacementFunction;
+};
 
-/*
-What is the capital of France?
-Paris + true
-Skope + false
-La Tour Eiffel + false
-Lyon + false
-*/
-
-createQuestion("What is the capital of France?", "SINGLE_QUESTION", [
-  { label: "Paris", correctAnswer: true },
-  { label: "Skope", correctAnswer: false },
-  { label: "La Tour Eiffel", correctAnswer: false },
-  { label: "Lyon", correctAnswer: false },
-]);
+export const createQuizz = ({
+  idGenerator,
+  getQuestions,
+  pickWithoutReplacementFunction,
+}: Dependencies): Quizz => ({
+  id: idGenerator(),
+  questions: getQuestions(pickWithoutReplacementFunction()),
+});
--- src/main_test.ts
+++ src/main_test.ts
@@ -1,13 +1,13 @@
-import { assertEquals, assertObjectMatch } from "@std/assert";
+import { assertObjectMatch } from "@std/assert";
 import {
   createQuizz,
-  getQuestionMocked,
   IdGenerator,
-  Quizz,
-  PickWithoutReplacement,
+  PickWithoutReplacementFunction,
+  type Question,
+  type Quizz,
 } from "./main.ts";
 
-const getQuestionMocked = () => ({
+const question1: Question = {
   id: 1,
   label: "What is the capital of France?",
   possibleAnswers: [
@@ -17,30 +17,42 @@ const getQuestionMocked = () => ({
     { id: 4, label: "Lyon", correctAnswer: false },
   ],
   typeOfQuestion: "SINGLE_QUESTION",
-});
+};
+const question4: Question = {
+  id: 4,
+  label: "What country is Helsinki the capital of ?",
+  possibleAnswers: [
+    { id: 1, label: "Denmark", correctAnswer: false },
+    { id: 2, label: "Sweden", correctAnswer: false },
+    { id: 3, label: "Finland", correctAnswer: true },
+    { id: 4, label: "Norway", correctAnswer: false },
+  ],
+  typeOfQuestion: "SINGLE_QUESTION",
+};
+
+const questions: Record<number, Question> = {
+  1: question1,
+  4: question4,
+};
+
+const getMockedQuestions = (ids: number[]): Question[] => {
+  return ids.map((id) => questions[id]);
+};
 
 Deno.test(function createQuizzTest() {
   // Given
-  const pickWithoutReplacement: PickWithoutReplacement = () => [1, 4];
+  const pickWithoutReplacement: PickWithoutReplacementFunction = () => [1, 4];
   const idGenerator: IdGenerator = () => 1;
   // When
-  const quizz = createQuizz(idGenerator, pickWithoutReplacement);
+  const quizz = createQuizz({
+    idGenerator,
+    getQuestions: getMockedQuestions,
+    pickWithoutReplacementFunction: pickWithoutReplacement,
+  });
   // Then
   const expectedQuizz: Quizz = {
     id: 1,
-    questions: [
-      {
-        id: 4,
-        label: "What is the capital of France?",
-        possibleAnswers: [
-          { id: 1, label: "Paris", correctAnswer: true },
-          { id: 2, label: "Skope", correctAnswer: false },
-          { id: 3, label: "La Tour Eiffel", correctAnswer: false },
-          { id: 4, label: "Lyon", correctAnswer: false },
-        ],
-        typeOfQuestion: "SINGLE_QUESTION",
-      },
-    ],
+    questions: [question1, question4],
   };
   assertObjectMatch(quizz, expectedQuizz);
 });
